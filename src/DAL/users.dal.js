a52eac18f93327836908ee1bed7415ca7f4e7e0b
Adrian Jarc <adrian.jarc@comland.si>
2019-03-31 14:35:40 +0200

    Add jsdoc comments

--- src/DAL/users.dal.js
+++ src/DAL/users.dal.js
@@ -1,11 +1,27 @@
 'use strict'
 
+/**
+ *
+ * @param {Object} user User to add to database
+ *
+ * @returns {Pormise} Was user succesfully saved
+ *
+ * @description Creates mongodb user and attempts to save it
+ */
 const createUser = user => {
   const userToAdd = new USERS(user)
 
   return userToAdd.save()
 }
 
+/**
+ *
+ * @param {String} email Email of user you weant to find
+ *
+ * @returns {Promise} Found user in db
+ *
+ * @description Find user in mongo db by email
+ */
 const findUserByEmail = email => USERS.findOne({ email }).lean().exec()
 
 module.exports = {
